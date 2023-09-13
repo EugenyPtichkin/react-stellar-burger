d381127be2e9656f4f233b6ed7ecb2e2149fe0b1
Eugeny Ptichkin <eugeny.ptichkin@novsu.ru>
2023-09-13 20:03:32 +0300

    исправлено чтение исходных данных из Api

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,44 @@
+import React from 'react';
 import styles from './app.module.css';
 import Header from '../header/header';
 import Main from '../main/main';
 
 export const isActive = false;
+const baseUrl = "https://norma.nomoreparties.space/api/ingredients";
 
-function App() {
+const App = () => {
+  const [data, setData] = React.useState({
+    ingredientsData: null,
+    isLoading: true
+  })
+
+  React.useEffect(() => {
+    const getIngredientsData = async () => {
+      try {
+        setData({ ...data, isLoading: true });
+        const res = await fetch(baseUrl);
+        const resultData = await res.json();
+        setData({
+          ...data,
+          ingredientsData: resultData.data,
+          isLoading: false
+        });
+      }
+      catch (err) {
+        console.log(err);
+      }
+    }
+    getIngredientsData();
+  }, [])
+  console.log(data.isLoading);
+  console.log(data.ingredientsData);
+
   return (
     <div className={styles.app}>
-      <Header/>
-      <Main/>
-    </div>
+      <Header />
+      {!data.isLoading && <Main ingredients={data.ingredientsData}/>}
+      {data.isLoading && <p>Данные загружаются</p>}
+    </div> 
   );
 }
 
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,26 +1,28 @@
 import styles from './main.module.css';
 import { BurgerIngredients } from './../burger-ingredients/burger-ingredients';
 import { BurgerConstructor } from './../burger-constructor/burger-constructor';
-import { data } from './../../utils/data';
+/*import { data } from './../../utils/data';*/
 
 export const bunsName = ['bun', 'Булки'];
 export const saucesName = ['sauce', 'Соусы'];
 export const mainsName = ['main', 'Начинки'];
 
-function Main() {
+function Main(props) {
+  /*console.log(props.ingredients);*/
   return (
     <div className={styles.main}>
       <section>
-        <BurgerIngredients props={data}>
+        <BurgerIngredients props={props.ingredients}>
         </BurgerIngredients>
-        
+
       </section>
 
       <section>
-        <BurgerConstructor props={data}>
+        <BurgerConstructor props={props.ingredients}>
         </BurgerConstructor>
       </section>
     </div>
+
   )
 }
 
