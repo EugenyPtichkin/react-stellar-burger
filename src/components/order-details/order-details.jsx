a579cf7f19c679d9d6eb156c88efc1894d73c514
Eugeny Ptichkin <eugeny.ptichkin@novsu.ru>
2023-11-15 21:04:33 +0300

    исправлена демонстрация старого заказа

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,17 +1,23 @@
 import Styles from './order-details.module.css';
 import imageDone from './../../images/done.png';
+import imageAlmostDone from './../../images/done.svg';
 import { useSelector } from 'react-redux';
 
-function OrderDetails() { 
-  const { orderNumber } = useSelector(store => store.order);
+function OrderDetails() {
+  const { orderNumber, orderRequest } = useSelector(store => store.order);
   return (
     <div className={Styles.order_details}>
-      <p className='text text_type_digits-large pb-8'>{orderNumber.toString().padStart(6, '0')}</p>
+
+      <p className='text text_type_digits-large pb-8'>
+        {orderRequest ? '000000' : orderNumber.toString().padStart(6, '0')} </p>
       <p className='text text_type_main-medium pb-15'>идентификатор заказа</p>
-      <img className='pb-15' src={imageDone}/>
+      {orderRequest ?
+        <img className='pb-15' src={imageAlmostDone} /> :
+        <img className='pb-15' src={imageDone} />
+      }
       <p className='text text_type_main-small pb-2'> Ваш заказ начали готовить</p>
       <p className='text text_type_main-small text_color_inactive'> Дождитесь готовности на орбитальной станции</p>
-    </div>    
+    </div>
   );
 }
 
